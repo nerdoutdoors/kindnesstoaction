b8e9839daf3c1b7b4492b8540451a2a299f710cf
Tyler Schindel <tschindel@live.com>
2020-12-24 12:07:13 -0800

    set up google maps via api key

--- src/app/_components/ambassador-and-coin/ambassador-and-coin.component.ts
+++ src/app/_components/ambassador-and-coin/ambassador-and-coin.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { MatFormFieldControl } from '@angular/material/form-field';
+import { Loader } from '@googlemaps/js-api-loader';
+
+let map: google.maps.Map;
+
+const loader = new Loader({
+  apiKey: 'AIzaSyClzIs4beJRuUasJRaY-M7notQHmre5UWQ',
+  version: 'weekly',
+});
 
 @Component({
   selector: 'app-ambassador-and-coin',
@@ -9,12 +17,16 @@ import { MatFormFieldControl } from '@angular/material/form-field';
 })
 
 export class AmbassadorAndCoinComponent implements OnInit {
-  sendEmail = () => {};
-  waiting = false;
+  public sendEmail = () => {};
 
   constructor() { }
 
   ngOnInit() {
+    loader.load().then(() => {
+      map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center: { lat: 41.850033, lng: -87.6500523 }, // approximate US center TODO: fit to bounds instead
+        zoom: 5,
+      });
+    });
   }
-
 }
