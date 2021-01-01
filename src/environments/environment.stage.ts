1f65b7957158fb2faa4a04aea633e394049490d3
Tyler Schindel <tschindel@live.com>
2021-01-01 15:17:32 -0800

    updated environment files

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
   baseUrl: window.location.origin, // Base URL here
-  apiUrl: 'http://stage.brn.rajoriya/api', // global API URL (for production build)
-  adminApiUrl: 'http://stage.brn.rajoriya/api/admin'
-  //... more of your variables
+  apiUrl: 'http://kindnesstoaction.org/api', // global API URL (for production build)
 };
